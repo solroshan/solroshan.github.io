acc62dfe92759b1fa16ca0604528416eba52178f
Gunes <75987087+solroshan@users.noreply.github.com>
2025-12-09 23:33:24 +0300

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,12 @@
+const Footer = () => (
+  <footer className="text-center mt-8 text-sm text-slate-500">
+    <nav className="flex justify-center space-x-6">
+      <a className="hover:underline" href="#">GrassCare Co.</a>
+      <a className="hover:underline" href="#">Yardım</a>
+      <a className="hover:underline" href="#">Şartlar</a>
+      <a className="hover:underline" href="#">Gizlilik Politikası</a>
+      <a className="hover:underline" href="#">Üyelik</a>
+    </nav>
+  </footer>
+);
+export default Footer;
