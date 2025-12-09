347ef15b9571b2c344bec21cdaae9fe2a3853623
Gunes <75987087+solroshan@users.noreply.github.com>
2025-12-09 08:19:14 +0300

    Add GrassMainFooter component

---
+++ grassfooter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Footer component
+const GrssMainFooter = () => (
+  <footer className="mt-8 text-center text-slate-500 text-sm">
+    <div className="max-w-6xl mx-auto">© GrassCare</div>
+  </footer>
+);
+
+export default GrassMainFooter;
