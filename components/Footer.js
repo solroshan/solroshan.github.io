c153c57f4ad29f11be5932572b70dce421398498
Gunes <75987087+solroshan@users.noreply.github.com>
2025-12-10 21:31:42 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,10 @@ const Footer = () => (
   <footer className="text-center mt-8 text-sm text-slate-500">
     <nav className="flex justify-center space-x-6">
       <a className="hover:underline" href="#">GrassCare Co.</a>
-      <a className="hover:underline" href="#">Yardım</a>
-      <a className="hover:underline" href="#">Şartlar</a>
-      <a className="hover:underline" href="#">Gizlilik Politikası</a>
-      <a className="hover:underline" href="#">Üyelik</a>
+      <a className="hover:underline" href="https://solroshan.github.io/yardim.html">Yardım</a>
+      <a className="hover:underline" href="https://solroshan.github.io/sartlar.html">Şartlar</a>
+      <a className="hover:underline" href="https://solroshan.github.io/gizlilik.html">Gizlilik Politikası</a>
+      <a className="hover:underline" href="https://solroshan.github.io/uyelik.html">Üyelik</a>
     </nav>
   </footer>
 );
