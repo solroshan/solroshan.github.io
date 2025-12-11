2a7c6f656c55038cd5ba67db56b336db85440594
Gunes <75987087+solroshan@users.noreply.github.com>
2025-12-11 16:40:43 +0300

    Create public-header.js

---
+++ components/public-header.js
@@ -0,0 +1,54 @@
+[Error] ReferenceError: Can't find variable: supabase
+	(anonim işlev) (react-dom.development.js:22888)
+	callCallback (react-dom.development.js:4151)
+	dispatchEvent
+	invokeGuardedCallbackDev (react-dom.development.js:4200)
+	invokeGuardedCallback (react-dom.development.js:4264)
+	reportUncaughtErrorInDEV (react-dom.development.js:22887)
+	captureCommitPhaseError (react-dom.development.js:27175)
+	commitPassiveMountEffects_complete (react-dom.development.js:24942)
+	commitPassiveMountEffects_begin (react-dom.development.js:24927)
+	commitPassiveMountEffects (react-dom.development.js:24915)
+	flushPassiveEffectsImpl (react-dom.development.js:27088)
+	flushPassiveEffects (react-dom.development.js:27033)
+	(anonim işlev) (react-dom.development.js:26818)
+	workLoop (react.development.js:2653)
+	flushWork (react.development.js:2626)
+	performWorkUntilDeadline (react.development.js:2920)
+[Error] The above error occurred in the <Header> component:
+
+Header@https://solroshan.github.io/dashboard.html:13:27
+div
+DashboardPage@https://solroshan.github.io/dashboard.html:112:27
+App
+
+Consider adding an error boundary to your tree to customize error handling behavior.
+Visit https://reactjs.org/link/error-boundaries to learn more about error boundaries.
+	logCapturedError (react-dom.development.js:18714)
+	(anonim işlev) (react-dom.development.js:18747)
+	callCallback (react-dom.development.js:15046)
+	commitUpdateQueue (react-dom.development.js:15067)
+	commitLayoutEffectOnFiber (react-dom.development.js:23440)
+	commitLayoutMountEffects_complete (react-dom.development.js:24737)
+	commitLayoutEffects_begin (react-dom.development.js:24723)
+	commitLayoutEffects (react-dom.development.js:24661)
+	commitRootImpl (react-dom.development.js:26872)
+	commitRoot (react-dom.development.js:26731)
+	performSyncWorkOnRoot (react-dom.development.js:26166)
+	flushSyncCallbacks (react-dom.development.js:12052)
+	flushPassiveEffectsImpl (react-dom.development.js:27109)
+	flushPassiveEffects (react-dom.development.js:27033)
+	(anonim işlev) (react-dom.development.js:26818)
+	workLoop (react.development.js:2653)
+	flushWork (react.development.js:2626)
+	performWorkUntilDeadline (react.development.js:2920)
+[Error] ReferenceError: Can't find variable: supabase
+	flushSyncCallbacks (react-dom.development.js:12066)
+	flushPassiveEffectsImpl (react-dom.development.js:27109)
+	flushPassiveEffects (react-dom.development.js:27033)
+	(anonim işlev) (react-dom.development.js:26818)
+	workLoop (react.development.js:2653)
+	flushWork (react.development.js:2626)
+	performWorkUntilDeadline (react.development.js:2920)
+> Seçilen Öğe
+< <html lang="tr">…</html>
